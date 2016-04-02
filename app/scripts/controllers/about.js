e8cbedfaac5e0cd8d8f7620eee30481dfadd9d31
tylerjxzhang <tylerzhang@outlook.com>
2016-04-01 21:19:37 -0400

    Yo angulared

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name finApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the finApp
+ */
+angular.module('finApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name finApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the finApp
+ */
+angular.module('finApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
